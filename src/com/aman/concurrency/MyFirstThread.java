049ee702f220f19ca904f6598d8a6a36c1c3e76d
amanuel <amannov21@gmail.com>
2023-07-20 11:04:28 -0600

    Thread sleep implementation , not to overburden resources we usually use sleep between 2 threads.

--- src/com/aman/concurrency/MyFirstThread.java
+++ src/com/aman/concurrency/MyFirstThread.java
@@ -1,15 +1,26 @@
 package com.aman.concurrency;
 
 
+import java.util.concurrent.TimeUnit;
 
 public class MyFirstThread {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Task task=new Task();
         Thread thread=new Thread(task);
-        thread.start();
+        thread.start(); //Once thread finishes , it changes to Terminted state so we cann't start or restart it
 
+        try {
+//            Thread.sleep(3000);   //main thread sleeps for 3 seconds , not to overburden resources
+                                       // disadvantage is unit of measure is only milli seconds
+            TimeUnit.SECONDS.sleep(3);
+
+        } catch (InterruptedException e){
+         e.printStackTrace();
+        }
 
         System.out.println("Inside main.....");
+
+
     }
 }
 
